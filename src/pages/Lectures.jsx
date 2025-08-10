382cb317ccf445afdcacc3b2ca532e60fa064b5a
KaluBhai-h <narendravadoliya93@gmail.com>
2025-08-10 15:08:25 +0530

    Update Lectures.jsx

--- src/pages/Lectures.jsx
+++ src/pages/Lectures.jsx
@@ -28,7 +28,9 @@ const Lectures = () => {
         { name: "Life Processes", index: 2,from:"Life Processes L1",to:"Life Processes L8 + Doubt Class" },
         { name: "Acid Base & Salt", index: 3,from:"Acid base and salts L1",to:"Doubt Solving Class" },
         { name: "Human eye & Colourful World", index: 4, from:"Human eye - L1",to:"Doubt Class" },
-        { name: "Control & Coordination", index:5, from:"Control and Co-ordination L1"},
+        { name: "Control & Coordination", index:5, from:"Control and Co-ordination L1", toid:"2204561"},
+        { name: "Metals & Non-Metals", fromid: "2205875", toid "2240797"},
+        { name: "Electricity", fromid: "2241947"},
       ]
     },
     Maths: {
